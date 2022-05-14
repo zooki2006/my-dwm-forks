97bb5fbcc7a063cad71b694b490a3eb95fbd92b7
zooki <fleshshell@cock.li>
2022-05-14 20:26:23 +0100

    clean up and shrink show.c

--- show.c
+++ show.c
@@ -1,19 +1,29 @@
+//void
+//ogshow(const Arg *arg)
+//{
+//	//if (selmon->hidsel)
+//	//	selmon->hidsel = 0;
+//	showwin(selmon->sel);
+//}
+
 void
 show(const Arg *arg)
 {
-//	if (selmon->hidsel)
-//		selmon->hidsel = 0;
-	showwin(selmon->sel);
-}
-
-void
-showwin(Client *c)
-{
-	if (!c)
+	if (!selmon->sel)
 		return;
 
-	XMapWindow(dpy, c->win);
-	setclientstate(c, NormalState);
-	arrange(c->mon);
+	//XMapWindow(dpy, selmon->sel->win);
+	setclientstate(selmon->sel, NormalState);
+	//arrange(selmon->sel->mon);
 }
+//void
+//showwin(Client *c)
+//{
+//	if (!c)
+//		return;
+//
+//	//XMapWindow(dpy, c->win);
+//	setclientstate(c, NormalState);
+//	//arrange(c->mon);
+//}
 
