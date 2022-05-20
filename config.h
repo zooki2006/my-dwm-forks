41198a021ee725a3303dbc4ad5d0558e00d9955b
zooki <fleshshell@cock.li>
2022-05-20 14:23:18 +0100

    add STATUSBAR for statusbar blocks

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+#define STATUSBAR "dwmblocks"
 /* static const int user_bh            = 16;      */  /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */ 
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
