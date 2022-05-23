82d4d84c4e5053b34902714417d115ca6ed4a1fd
zooki <fleshshell@cock.li>
2022-05-23 22:30:08 +0100

    remove nexttiled as it is now useless

--- dwm.c
+++ dwm.c
@@ -180,7 +180,6 @@ static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
-//static Client *nexttiled(Client *c);
 //static void pop(Client *);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
@@ -1202,12 +1201,6 @@ movemouse(const Arg *arg)
 	}
 }
 
-/* Client *
-nexttiled(Client *c)
-{
-	for (; c && (c->isfloating || !ISVISIBLE(c)); c = c->next);
-	return c;
-} */
 
 /*
 void
