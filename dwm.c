1a2ba90b2354dc2bdb23f7c4ed900762d1185f6e
zooki <fleshshell@cock.li>
2022-05-23 18:44:20 +0100

    remove layout from statusbar

--- dwm.c
+++ dwm.c
@@ -741,9 +741,10 @@ drawbar(Monitor *m)
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		x += w;
 	}
-	w = blw = TEXTW(m->ltsymbol);
+	//w = blw = TEXTW(tags[i]);
+	w = blw = TEXTW("");
 	drw_setscheme(drw, scheme[SchemeNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
+//	x = drw_text(drw, x, 0, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (n > 0) {
