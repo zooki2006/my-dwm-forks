b0fbc7d055691964d34d9dc971d90c764d05f987
zooki <fleshshell@cock.li>
2022-05-23 18:50:07 +0100

    fix silly mistake

--- dwm.c
+++ dwm.c
@@ -2173,11 +2173,8 @@ center(const Arg *arg){
 	Client *c = selmon->sel;
 	if(!c)
 		return;
-	if (c->isfloating)
-	{
-		c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
-		c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
-	}
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	XMoveWindow(dpy, c->win, c->x, c->y);
 }
 
