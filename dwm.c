540284ea0ed18579d523e3c8a78062600a7ec544
zooki <fleshshell@cock.li>
2022-05-23 22:30:20 +0100

    remove monocle layput

--- dwm.c
+++ dwm.c
@@ -1108,22 +1108,7 @@ maprequest(XEvent *e)
 	if (!wintoclient(ev->window))
 		manage(ev->window, &wa);
 }
-/*
-void
-monocle(Monitor *m)
-{
-	unsigned int n = 0;
-	Client *c;
 
-	for (c = m->clients; c; c = c->next)
-		if (ISVISIBLE(c))
-			n++;
-	if (n > 0) // override layout symbol 
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
-	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
-}
-*/
 void
 motionnotify(XEvent *e)
 {
