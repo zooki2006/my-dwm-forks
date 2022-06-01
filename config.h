24c00a077d879ef42dd82a662a9e79e6749452d5
zooki <fleshshell@cock.li>
2022-06-01 15:39:51 +0100

    remove old binds to clean up

--- config.h
+++ config.h
@@ -314,45 +314,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_Escape,      quit,           {0} },
-	/* old binds remove later ?*/
-/*	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
- *	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
- *	{ MODKEY,                       XK_b,      togglebar,      {0} },
- *	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
- *	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
- *	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
- *	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
- * 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
- *	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
- *	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
- *	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
- *	{ MODKEY,                       XK_Return, zoom,           {0} },
- *	{ MODKEY,                       XK_Tab,    view,           {0} },
- *	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
- *	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
- *	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
- *	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
- *	{ MODKEY,                       XK_space,  setlayout,      {0} },
- *	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
- *	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
- *	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
- * 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
- *	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
- *	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
- *	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
- *	{ MODKEY,            			XK_y,  	   togglescratch,  {.ui = 0 } },
- *	{ MODKEY,            			XK_u,	   togglescratch,  {.ui = 1 } },
- *	{ MODKEY,            			XK_x,	   togglescratch,  {.ui = 2 } },
- *	TAGKEYS(                        XK_1,                      0)
- *	TAGKEYS(                        XK_2,                      1)
- *	TAGKEYS(                        XK_3,                      2)
- *	TAGKEYS(                        XK_4,                      3)
- *	TAGKEYS(                        XK_5,                      4)
- *	TAGKEYS(                        XK_6,                      5)
- *	TAGKEYS(                        XK_7,                      6)
- *	TAGKEYS(                        XK_8,                      7)
- *	TAGKEYS(                        XK_9,                      8)
- *	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },i */
 };
 
 /* button definitions */
